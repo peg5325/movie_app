95edb0501b98259d2090edd17a3257024558f0f4
Eongyu Park <peg5325@gmail.com>
2021-06-06 05:42:21 +0900

    I don't know what is diff

--- src/App.js
+++ src/App.js
@@ -8,21 +8,19 @@ class App extends React.Component {
     isLoading: true,
     movies: [],
   };
-
   getMovies = async () => {
     const {
       data: {
         data: { movies },
       },
     } = await axios.get(
-      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
     );
     this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
-
   render() {
     const { isLoading, movies } = this.state;
     return (
